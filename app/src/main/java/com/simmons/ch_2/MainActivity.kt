e57115246676efaf8167292f39f7a822f4ed2e2e
skimmonsmsu <sms576s@missouristate.edu>
2025-09-10 12:35:16 -0500

    Chapter 2 stuck

--- app/src/main/java/com/simmons/ch_2/MainActivity.kt
+++ app/src/main/java/com/simmons/ch_2/MainActivity.kt
@@ -4,20 +4,34 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.simmons.ch_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var trueButton : Button
     private lateinit var falseButton : Button
 
+    private lateinit var binding : ActivityMainBinding
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        //trueButton = findViewById(R.id.true_button)
+        //falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener {
+        binding.trueButton.setOnClickListener {
             Toast.makeText(
                 this,
                 R.string.correct_toast,
@@ -25,7 +39,7 @@ class MainActivity : AppCompatActivity() {
             )
             .show()
         }
-        falseButton.setOnClickListener {
+        binding.falseButton.setOnClickListener {
            Toast.makeText(
                this,
                R.string.incorrect_toast,
